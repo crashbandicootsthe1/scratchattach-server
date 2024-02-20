db6864b11eae06ddb1bc6ab0fa5ba3dec3adebc3
crashbandicootsthe1 <144353514+crashbandicootsthe1@users.noreply.github.com>
2024-02-20 09:47:59 +0400

    First version

---
+++ local-storage.py
@@ -0,0 +1,54 @@
+import scratchattach as scratch3
+import json
+import os
+
+session = scratch3.Session(
+    os.environ["SESSION_ID"], username="crashbandicootsthe1")
+conn = session.connect_cloud(968656181)
+client = scratch3.CloudRequests(conn)
+
+
+@client.request
+def save_key(username: str, key_name: str, key_value, json_file: str = "storage-data.json"):
+    try:
+        with open(json_file, 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        # If the file doesn't exist, create an empty dictionary
+        data = {}
+
+    # If the username doesn't exist in the data, create a new entry
+    if username not in data:
+        data[username] = {}
+
+    # Add or update the key-value pair for the username
+    data[username][key_name] = key_value
+
+    # Write the updated data back to the JSON file
+    with open(json_file, 'w') as file:
+        json.dump(data, file, indent=4)
+        return "Done"
+
+
+@client.request
+def load_keys(username=None):
+    if username is None:
+        username = session.get_linked_user()
+    with open('storage-data.json', 'r') as file:
+        data = json.load(file)
+        user_data = data.get(username, {})
+        key_value_pairs = [item for pair in user_data.items() for item in pair]
+        return key_value_pairs
+
+
+@client.request
+def ping():
+    return "pong"
+
+
+@client.event
+def on_ready():
+    print("Request handler started.")
+
+
+client.run(no_packet_loss=True)
